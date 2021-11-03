61532a1b3470b1725658e1eecc8e2a742cae2fad
hizawadai <danihizawa@gmail.com>
2021-11-04 03:07:59 +0700

    pustbok

---
+++ application/controllers/biodata.php
@@ -0,0 +1,13 @@
+<?php
+class biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo " <h1>Perkenalan</h1>";
+        echo"nama saya Hillzawa Danny 
+            saya tinggal di daerah surakarta 
+            hobi saya adalah membuat digital art
+            saya juga suka membaca buku berbau teknologi";
+        
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Hasan</li>
+        <li>Nama Belakang : Basri</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Pondok Wage Indah 1 i-7</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Surabaya</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Renang</li>
+        <li>Lari</li>
+    </ul>
+</section>
